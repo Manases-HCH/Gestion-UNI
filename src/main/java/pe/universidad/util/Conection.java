607943b336e5643a7e2b1c44717fc5e22b76dd6c
Manases Huaman Churo <manahuaman1504@gmail.com>
2025-11-19 09:57:47 -0500

    MySQL con Railway

--- src/main/java/pe/universidad/util/Conection.java
+++ src/main/java/pe/universidad/util/Conection.java
@@ -1,37 +1,42 @@
-
 package pe.universidad.util;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-
-/**
- *
- * @author Estudiante
- */
 public class Conection {
     private Connection cnx;
-    
+
     public Connection conecta() throws ClassNotFoundException {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            String usr = "root";
-            String psw = "";
-            String url = "jdbc:mysql://localhost:3306/bd-uni";
-            cnx = DriverManager.getConnection(url, usr, psw);
+
+            String host = "nozomi.proxy.rlwy.net";
+            String port = "58948";
+            String database = "railway";
+            String user = "root";
+            String password = "WqtWQlNBfsYwEputBlISihwPziyRWCfI";
+
+            String url = "jdbc:mysql://" + host + ":" + port + "/" + database
+                    + "?allowPublicKeyRetrieval=true"
+                    + "&useSSL=false"
+                    + "&requireSSL=false"
+                    + "&serverTimezone=UTC";
+
+            cnx = DriverManager.getConnection(url, user, password);
+            System.out.println("✔ Conexión Exitosa");
             return cnx;
+
         } catch (SQLException e) {
-            System.out.println(e.getMessage());
+            System.out.println("❌ ERROR CONEXIÓN: " + e.getMessage());
+            e.printStackTrace();
         }
         return null;
     }
-    
+
     public int pruebaConexion() throws ClassNotFoundException {
         Connection c = conecta();
-        if (c!= null) {
-            return 1;
-        } else {
-            return 0;
-        }
-    }    
+        return (c != null) ? 1 : 0;
+    }
 }
+
+
